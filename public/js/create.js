858f7f5685dbdcb81b39a99aa09169d931336dad
adarsh shrivatava <52086171+shrivastavadarsh@users.noreply.github.com>
2021-05-18 15:56:15 +0530

    New Version :
    With a lot of change_database almost implemented

---
+++ public/js/create.js
@@ -0,0 +1,38 @@
+function getRandomString(length) {
+    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    for ( var i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+}
+
+const dbBtn1 = document.getElementById('dbBtn1');
+var user, key;  
+var flag = true;
+
+dbBtn1.addEventListener('click', e => {
+    user = document.getElementById('username').value;
+    key = getRandomString(9);
+    document.getElementById('room').value = key;
+    document.getElementById('dbBtn1').innerHTML = "Registered" ;
+    document.getElementById('dbBtn1').disabled = true;
+    // flag = true;
+    e.preventDefault();
+    db.collection('users').add({
+        username: user,
+        roomID: key,
+        admin:1
+    });
+});
+
+function go1() {
+    if(flag){
+        document.getElementById('form1').action = 'chat.html';
+        // flag = false;
+    }
+    else{
+        alert('Please Register Yourself');
+    }
+}
+
---
+++ public/js/join.js
@@ -0,0 +1,29 @@
+const dbBtn2 = document.getElementById('dbBtn2');
+var user, key;  
+var flag = true;
+
+dbBtn2.addEventListener('click', e => {
+    user = document.getElementById('username').value;
+    key = document.getElementById('room').value;
+    // flag = true;
+    document.getElementById('dbBtn2').innerHTML = "Registered" ;
+    document.getElementById('dbBtn2').disabled = true;
+    e.preventDefault();
+    db.collection('users').add({
+        username: user,
+        roomID: key,
+        admin:0
+    });
+});
+
+function go2() {
+    if(flag){
+        
+        
+        document.getElementById('form2').action = 'chat.html';
+        // flag = false;
+    }
+    else{
+        alert('Please Register Yourself');
+    }
+}
